ee0fd04ddc1bbd0038c86442641a92cdc8a04262
Michael Shi <mfshi03@vt.edu>
2023-12-06 22:27:02 -0500

    deleting previous evaluate and setting dataset size to 50000

--- data/text2code_loader.py
+++ data/text2code_loader.py
@@ -3,7 +3,7 @@ from datasets import load_dataset
 dataset = load_dataset("code_x_glue_tc_text_to_code", split="train")
 count = 0
 for data in dataset:
-    if count > 10000:
+    if count > 50000:
         break
     with open("code.txt", "a") as f:
         s = data["nl"] + "[SEP]" + data["code"] + "\n"
